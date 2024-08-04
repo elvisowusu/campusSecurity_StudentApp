8a8947428da1e410ef356a8ade77e11552751dec
elvisowusu <owusuelvisgyasi@gmail.com>
2024-08-04 00:02:49 +0000

    completed reset password feature

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -11,8 +11,17 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
-  TextEditingController emailController = TextEditingController();
-  final auth = FirebaseAuth.instance;
+  final TextEditingController emailController = TextEditingController();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FocusNode _emailFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    _emailFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,15 +45,18 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
   void myDialogBox(BuildContext context) {
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return Dialog(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              padding: const EdgeInsets.all(20),
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -64,49 +76,92 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                           Navigator.pop(context);
                         },
                         icon: const Icon(Icons.close),
-                      )
+                      ),
                     ],
                   ),
                   const SizedBox(
                     height: 20,
                   ),
-                  TextField(
+                  TextFormField(
                     controller: emailController,
+                    focusNode: _emailFocusNode,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       labelText: "Enter the Email",
-                      hintText: "eg name@gmail.com",
+                      hintText: "e.g., name@gmail.com",
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.red),
+                      ),
                     ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Email can't be empty";
+                      }
+                      // Regular expression for validating email addresses
+                      final emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+                      if (!emailRegex.hasMatch(value)) {
+                        return "Invalid email format";
+                      }
+                      return null;
+                    },
                   ),
                   const SizedBox(
                     height: 20,
                   ),
                   ElevatedButton(
-                      onPressed: () async {
-                        await auth
-                            .sendPasswordResetEmail(email: emailController.text)
-                            .then((value) {
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        try {
+                          await auth.sendPasswordResetEmail(email: emailController.text);
                           Fluttertoast.showToast(
-                              msg: "Reset Password link sent to your email");
-                        }).onError((error, stackTrace) {
-                          Fluttertoast.showToast(msg: error.toString());
-                        });
-                        Navigator.pop(context);
-                        emailController.clear();
-                      },
-                      child: const Text(
-                        "Send",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      ))
+                            msg: "Reset Password link sent to your email",
+                          );
+                          Navigator.pop(context);
+                          emailController.clear();
+                        } on FirebaseAuthException catch (e) {
+                          // Handle specific Firebase authentication exceptions
+                          if (e.code == 'user-not-found') {
+                            Fluttertoast.showToast(
+                              msg: "No user found with this email.",
+                            );
+                          } else if (e.code == 'invalid-email') {
+                            Fluttertoast.showToast(
+                              msg: "The email address is badly formatted.",
+                            );
+                          } else {
+                            // Handle other FirebaseAuthException codes if needed
+                            Fluttertoast.showToast(
+                              msg: "An error occurred. Please try again.",
+                            );
+                          }
+                        } catch (error) {
+                          Fluttertoast.showToast(
+                            msg: "An unexpected error occurred. Please try again.",
+                          );
+                        }
+                      } else {
+                        // Focus the email field if it's invalid
+                        _emailFocusNode.requestFocus();
+                      }
+                    },
+                    child: const Text(
+                      "Send",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
